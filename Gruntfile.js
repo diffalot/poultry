39b09f88c6f0b8075035c12e8d195cbc5bf4cadc
Andrew Davis <andrew@lightcorp.net>
2013-12-03 22:39:01 -0600

    use `grunt` to run the server and watch for changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,19 @@ module.exports = function(grunt) {
     clean: ['dist/'],
     browserify: {
       'dist/bookmarklet.js': ['bookmarklet.js']
+    },
+    watch: {
+      css: {
+          files: '*.js',
+          tasks: ['browserify', 'forever:restart']
+        }
     }
   });
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-forever');
 
-  grunt.registerTask('default', ['clean', 'browserify']);
+  grunt.registerTask('default', ['clean', 'browserify', 'forever:start', 'watch']);
 }
